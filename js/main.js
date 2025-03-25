bc9cbf0bee253861802abacafe4ad32fbac71a33
solcassy <msanchezcas@centro.edu.mx>
2025-03-25 09:52:42 -0600

    font

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log("Hola mundo")
+document.addEventListener('DOMContentLoaded', () => {
+    const h1 = document.querySelector('h1');
+
+    h1.addEventListener('mouseover', () => {
+        const randomX = Math.floor(Math.random() * 1000) - 500; // Random value between -250 and 250
+        const randomY = Math.floor(Math.random() * 1000) - 500; // Random value between -250 and 250
+        h1.style.transform = `translate(${randomX}px, ${randomY}px)`;
+    });
+
+    h1.addEventListener('mouseout', () => {
+        h1.style.transform = 'translate(0, 0)';
+    });
+});
